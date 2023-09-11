7d9a4d47a27ea8d62edae772502999f4d124af48
zjy4fun <zjy4fun@gmail.com>
2023-09-11 18:12:45 +0800

    feat: add lint-staged

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- tailwindcss 实现水平垂直居中，加上红颜色的文字 -->
-  <div class="flex justify-center items-center text-red-500">
-    Hello Tailwind CSS
-  </div>
+  <div class="flex justify-center items-center text-blue-500 font-bold">Hello Tailwind CSS</div>
+  <HelloWorld msg="Vue 3 + Vite + Tailwind CSS" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
