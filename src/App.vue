651e62e9e3e4d3b337f277597d3059a1417b6c44
zjy4fun <zjy4fun@gmail.com>
2023-09-11 17:32:53 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!-- tailwindcss 实现水平垂直居中，加上红颜色的文字 -->
+  <div class="flex justify-center items-center text-red-500">
+    Hello Tailwind CSS
+  </div>
+</template>
+
+<style scoped>
+
+</style>
